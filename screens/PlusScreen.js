9dbf181a0e9108884ca4bb316fce9c1983ad5a4b
AndrewMochalskyi <42970249+AndrewMochalskyi@users.noreply.github.com>
2020-05-07 05:45:32 +0300

    service

--- screens/PlusScreen.js
+++ screens/PlusScreen.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
+import  FoodList from '../components/FoodList';
 
 export default function PlusScreen({ navigation }) {
   return (
